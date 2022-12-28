ec2f014400a039472045fd1c7057ddd374b5f840
David Henry <davidhenrydev@gmail.com>
2022-12-28 16:51:11 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,98 @@
+import "./style.css";
+import Barber from "./components/Barber";
+import Header from "./components/Header";
+
+
+export default function App() {
+  
+    return (
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8"/>
+            <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+            <link rel="stylesheet" href="css/normalize.css"/>
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+                <link href="https://fonts.googleapis.com/css2?family=Spectral&display=swap" rel="stylesheet"/>
+            <title>Hair Salon</title>
+        </head>
+        <body>
+          <Header />
+            {/* Main hero section */}
+            <main>
+                <div class = "container main">
+                <div class ="headline">
+                    <h1>BLONDE & HIGHLIGHT</h1>
+                    <span>Divide your uniqueness by your hairs in style</span>
+                    <button><a href="#">Our Services</a></button>
+                </div>
+                <div class ="bookNow">
+                    <span>Your First Visit?</span>
+                    <p>-20% for the first classic hair coloring</p>
+                    <button><a href="#">- Book Online</a></button>
+                </div>
+            </div>
+            </main>
+            {/* Info & about sections */}
+            <section class="infoSect">
+                <div class="container">
+                <div>
+                    <i class="fa-solid fa-scissors"></i>
+                    <h4>EXPERT BEAUTICIANS</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est suscipit iure, sequi consectetur similique architecto.</p>
+                </div>
+                <div>
+                    <i class="fa-solid fa-mortar-pestle"></i>
+                    <h4>QUALITY SERVICE</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus deserunt reprehenderit doloribus magnam, eaque quos!</p>
+                </div>
+                <div>
+                    <i class="fa-solid fa-ruler"></i>
+                    <h4>BEAUTY PRODUCTS</h4>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum perferendis, aliquam excepturi saepe numquam ut!</p>
+                </div>
+                </div>
+            </section>
+            {/* Video preview section */}
+            <section class="vidSect">
+                <div class="container">
+                    <section>
+                    <section><img src="/images/cuttingHair.jpg" alt=""/>
+                    <h2>MAKE YOUR OWN MOMENTUM</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus explicabo sapiente amet fugit, voluptatibus error tempore in id. Sequi error ullam eum praesentium earum, alias vero eius maiores iste sit.</p>
+                    <button><a href="#">- READ MORE</a></button>
+                </section>
+                </section>
+            </div>
+            </section>
+            {/* stylist section */}
+            <section class="stylistSect">
+                <div class="container">
+                <Barber />
+                <section><img src="/images/barber2.jpg" alt=""/></section>
+                <section><img src="/images/barber3.jpg" alt=""/></section>
+            </div>
+            </section>
+            {/* Offers section */}
+            <section class="offerSect">
+                <div class="container">
+                <section>
+                <span>COLOUR DAY OFFER!</span>
+                <p>-10% ON HAIRCUT COLOR AND HIGHLIGHT</p>
+                <button><a href="#">- BOOK APPOINTMENT</a></button>
+                </section>
+                <img src="https://res.cloudinary.com/djlowrayn/image/upload/v1645448981/hairSalon/haircut_hqn2wx.png" alt=""/>
+                </div>
+            </section>
+            {/* Gallery sections */}
+               <section class="gallerySect">
+            <div class="container">
+                    <h1>OUR LOOKBOOK</h1>
+                    <img src="https://res.cloudinary.com/djlowrayn/image/upload/v1646329860/hairSalon/gallery_h4te8g.png" alt=""/>
+                </div>
+                </section>
+        </body>
+        </html>
+        
+    );
+  }
---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+
+
+export default function Header() {
+    return(
+        <header>
+            <nav>
+                <img src="https://res.cloudinary.com/djlowrayn/image/upload/c_scale,w_200/v1653853880/hairSalon/logo2_aiaexh.png" class = "logo" alt=""/>
+                <ul>
+                <li><a href="#">ABOUT</a></li>
+                <li><a href="#">SERVICES</a></li>
+                <li><a href="#">LOOKBOOK</a></li>
+                <li><a href="#">CONTACT</a></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
